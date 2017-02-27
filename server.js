d52833c5877bd9ac9b4e5dd91241287d99df36a3
tranwald <smyks@tlen.pl>
2017-02-27 15:59:20 +0000

    [TIMESTAMP][API] Complete timestamp server

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-app.get('/home', (req, res) => {
-    res.writeHeader(200, {
-        'Content-Type': 'text/plain'
+const dateToNatural = (date) => `${MONTHS[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`;
+
+const getDateObject = (date) => {
+    const parsedDate = +date || Date.parse(date);
+    const dateObj = new Date((+date || parsedDate));
+    return Number.isSafeInteger(parsedDate) ? {
+        unix: +dateObj,
+        natural: dateToNatural(dateObj)
+    } : {
+        unix: null,
+        natural: null
+    };
+};
+
+app.get('/:date', (req, res) => {
+    res.status(200)
+        .json(getDateObject(req.params.date));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
-    res.end('Hello World!');
-}).listen(8080);
+});
+
+app.listen(8080);
